b4a00fd6a9eeb295a960bbf696811fd4fa32443a
Matthew O'Connell <totallyterrific@tutanota.com>
2023-05-17 19:45:46 +0200

    refactor(020_Graph_init.ts): comment out

--- evm/deploy/020_Graph_Init.ts
+++ evm/deploy/020_Graph_Init.ts
@@ -1,60 +1,60 @@
-import fs from "fs";
-import { DeployFunction } from "hardhat-deploy/types";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+// import fs from "fs";
+// import { DeployFunction } from "hardhat-deploy/types";
+// import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-import {
-  Baal,
-  BaalAdvTokenSummoner,
-  BaalAndVaultSummoner,
-  BaalSummoner,
-  DeVoxShaman,
-  DeVoxShamanSummoner,
-} from "../src/util";
+// import {
+//   Baal,
+//   BaalAdvTokenSummoner,
+//   BaalAndVaultSummoner,
+//   BaalSummoner,
+//   DeVoxShaman,
+//   DeVoxShamanSummoner,
+// } from "../src/util";
 
-async function runTask(
-  task: "init" | "add",
-  contractName: string,
-  hre: HardhatRuntimeEnvironment,
-  additionalArgs?: any[],
-) {
-  const { deployments } = hre; // we get the deployments and getNamedAccounts which are provided by hardhat-deploy.
-  const { get } = deployments; // The deployments field itself contains the deploy function.
+// async function runTask(
+//   task: "init" | "add",
+//   contractName: string,
+//   hre: HardhatRuntimeEnvironment,
+//   additionalArgs?: any[],
+// ) {
+//   const { deployments } = hre; // we get the deployments and getNamedAccounts which are provided by hardhat-deploy.
+//   const { get } = deployments; // The deployments field itself contains the deploy function.
 
-  const { address } = await get(contractName);
-  const args = {
-    contractName,
-    address,
-    ...additionalArgs
-  };
+//   const { address } = await get(contractName);
+//   const args = {
+//     contractName,
+//     address,
+//     ...additionalArgs
+//   };
 
-  console.log("cwd", process.cwd());
+//   console.log("cwd", process.cwd());
 
-  const path = `./subgraph/generated/${contractName}/${contractName}.ts`;
-  if (fs.existsSync(path)) {
-    console.log(`${path} exists, running update task`);
-    await hre.run("update", args);
-  } else {
-    console.log(`${path} does not exist, running ${task} task`);
-    await hre.run(task, args);
-  }
-}
+//   const path = `./subgraph/generated/${contractName}/${contractName}.ts`;
+//   if (fs.existsSync(path)) {
+//     console.log(`${path} exists, running update task`);
+//     await hre.run("update", args);
+//   } else {
+//     console.log(`${path} does not exist, running ${task} task`);
+//     await hre.run(task, args);
+//   }
+// }
 
-const init = async (contractName: string, hre: HardhatRuntimeEnvironment) => {
-  await runTask("init", contractName, hre);
-};
+// const init = async (contractName: string, hre: HardhatRuntimeEnvironment) => {
+//   await runTask("init", contractName, hre);
+// };
 
-const add = async (contractName: string, hre: HardhatRuntimeEnvironment) => {
-  await runTask("add", contractName, hre);
-};
+// const add = async (contractName: string, hre: HardhatRuntimeEnvironment) => {
+//   await runTask("add", contractName, hre);
+// };
 
-const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  await init(DeVoxShaman, hre);
-  // await add(DeVoxShamanSummoner, hre);
-  // await add(Baal, hre);
-  // await add(BaalSummoner, hre);
-  // await add(BaalAdvTokenSummoner, hre);
-  // await add(BaalAndVaultSummoner, hre);
-};
+// const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+//   await init(DeVoxShaman, hre);
+//   await add(DeVoxShamanSummoner, hre);
+//   await add(Baal, hre);
+//   await add(BaalSummoner, hre);
+//   await add(BaalAdvTokenSummoner, hre);
+//   await add(BaalAndVaultSummoner, hre);
+// };
 
-export default deploy;
-deploy.tags = ["graph", "local", "staging"];
+// export default deploy;
+// deploy.tags = ["graph", "local", "staging"];
