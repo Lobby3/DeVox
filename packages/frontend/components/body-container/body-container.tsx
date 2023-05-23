a817ef79e4366b8d3007946af139f61fda63efe6
jipstavenuiter <jipstavenuiter@gmail.com>
2023-05-23 17:07:02 +0200

    center body layout

--- packages/frontend/components/body-container/body-container.tsx
+++ packages/frontend/components/body-container/body-container.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable-next-line */
-import { Center } from "@chakra-ui/react";
+import { Center, Flex } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
 export function BodyContainer({ children }: PropsWithChildren) {
   return (
-    <Center maxWidth="1360px" padding="80px 44px" width="100%">
-      {children}
+    <Center width="100vw">
+      <Flex maxWidth="1360px" padding="80px 44px" width="100%">
+        {children}
+      </Flex>
     </Center>
   );
 }
