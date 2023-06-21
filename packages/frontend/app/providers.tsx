ab199585f1708593064fed011d90f7ccc5b98dfe
jipstavenuiter <jipstavenuiter@gmail.com>
2023-06-21 14:08:11 +0200

    fix server side rendering issues

--- packages/frontend/app/providers.tsx
+++ packages/frontend/app/providers.tsx
@@ -2,22 +2,16 @@
 
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
-import { HausConnectProvider } from "@daohaus/daohaus-connect-feature";
-import { HausThemeProvider } from "@daohaus/ui";
 
 import { theme } from "../styles/theme";
 import { MagicWalletProvider } from "./magic-wallet-context";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <HausThemeProvider>
-      <HausConnectProvider>
-        <CacheProvider>
-          <MagicWalletProvider>
-            <ChakraProvider theme={theme}>{children}</ChakraProvider>
-          </MagicWalletProvider>
-        </CacheProvider>
-      </HausConnectProvider>
-    </HausThemeProvider>
+    <CacheProvider>
+      <MagicWalletProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </MagicWalletProvider>
+    </CacheProvider>
   );
 }
--- packages/frontend/app/summon/page.tsx
+++ packages/frontend/app/summon/page.tsx
@@ -1,84 +1,12 @@
 "use client";
 
-import { TXBuilder } from "@daohaus/tx-builder";
-import { widthQuery } from "@daohaus/ui";
-import styled from "@emotion/styled";
-import { useWeb3React } from "@web3-react/core";
-import { useState } from "react";
+import dynamic from "next/dynamic";
 import React from "react";
 
-import { CenterLayout } from "./layouts/FormLayouts";
-import { SummonError } from "./layouts/SummonError";
-import { SummonerForm } from "./layouts/SummonerForm";
-import { SummonerLoading } from "./layouts/SummonerLoading";
-import { SummonerSuccess } from "./layouts/SummonerSuccess";
-import { SummonStates } from "./types";
-import { hexadecimalize } from "./utils";
+const DynamicContent = dynamic(() => import("./Content"), {
+  ssr: false,
+});
 
-const TemporaryLayout = styled.div`
-  background-color: #1a1a1a;
-  width: 100%;
-  padding-top: 2.7rem;
-  padding: 4rem;
-  @media ${widthQuery.sm} {
-    .title {
-      font-size: 3.5rem;
-      margin-bottom: 2rem;
-    }
-  }
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  label {
-    color: #fff;
-  }
-`;
-
-const SummonerPage = () => {
-  // TODO: Wire this up with magic.link
-  // TODO: Summon at the same time
-  //
-  const { provider, chainId } = useWeb3React();
-  const [summonState, setSummonState] = useState<SummonStates>("idle");
-  const [txHash, setTxHash] = useState<string>("");
-  const [daoAddress, setDaoAddress] = useState<string>("");
-  const [errMsg, setErrMsg] = useState<string>("");
-
-  const chainIdHex = hexadecimalize(chainId);
-  return (
-    <TXBuilder provider={provider} chainId={chainIdHex} appState={{}}>
-      <TemporaryLayout>
-        <CenterLayout>
-          {summonState === "idle" && (
-            <SummonerForm
-              setSummonState={setSummonState}
-              setTxHash={setTxHash}
-              setDaoAddress={setDaoAddress}
-              setErrMsg={setErrMsg}
-            />
-          )}
-          {summonState === "loading" && <SummonerLoading txHash={txHash} />}
-          {summonState === "success" && (
-            <SummonerSuccess
-              chainId={chainIdHex}
-              daoAddress={daoAddress}
-              setSummonState={setSummonState}
-            />
-          )}
-          {summonState === "error" && (
-            <SummonError
-              errMsg={errMsg}
-              setSummonState={setSummonState}
-              daoAddress={daoAddress}
-            />
-          )}
-        </CenterLayout>
-      </TemporaryLayout>
-    </TXBuilder>
-  );
-};
-
-export default SummonerPage;
+export default async function Index() {
+  return <DynamicContent />;
+}
