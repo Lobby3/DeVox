838053db083e4d09c4c92aec7f7b69939e50dab6
jipstavenuiter <jipstavenuiter@gmail.com>
2023-06-28 11:18:03 +0200

    add only-my-donations toggle

--- packages/frontend/components/donations-overview/donations-overview.tsx
+++ packages/frontend/components/donations-overview/donations-overview.tsx
@@ -1,12 +1,15 @@
 import {
   Center,
+  Flex,
   Heading,
   List,
   ListIcon,
   ListItem,
+  Switch,
   Text,
 } from "@chakra-ui/react";
 import { CircleStackIcon } from "@heroicons/react/24/outline";
+import { useWeb3React } from "@web3-react/core";
 import { formatUnits } from "ethers/lib/utils";
 import React from "react";
 
@@ -28,6 +31,8 @@ export function DonationsOverview({ campaignId }: DonationsOverviewProps) {
     isError,
   } = useGetDonationsForCampaign(campaignId);
   const { decimals, symbol } = useTokenInfo(campaign?.tokenAddress);
+  const { isActive, account } = useWeb3React();
+  const [onlyShowMyDonations, setOnlyShowMyDonations] = React.useState(false);
 
   if (isLoading) {
     return <Loader />;
@@ -46,26 +51,47 @@ export function DonationsOverview({ campaignId }: DonationsOverviewProps) {
   }
 
   return (
-    <List>
-      {donations.map((donation) => {
-        const formattedAmount = formatUnits(donation.amount, decimals);
-        const formattedMessage = donation.message?.text
-          ? ` "${donation.message.text}"`
-          : "";
+    <>
+      {isActive && (
+        <Flex alignItems={"center"}>
+          <Switch
+            checked={onlyShowMyDonations}
+            onChange={(e) => setOnlyShowMyDonations(!!e.target.value)}
+          />
+          <Text ml={2}>Only show my own donations</Text>
+        </Flex>
+      )}
+      <List>
+        {donations
+          .filter((donation) =>
+            isActive && onlyShowMyDonations && account
+              ? donation.user.id === account.toLowerCase()
+              : true
+          )
+          .map((donation) => {
+            const formattedAmount = formatUnits(donation.amount, decimals);
+            const formattedMessage = donation.message?.text
+              ? ` "${donation.message.text}"`
+              : "";
 
-        return (
-          <ListItem key={donation.id} display={"flex"} alignItems={"center"}>
-            <ListIcon as={CircleStackIcon} color="green.500" />
-            <Text>
-              <b>
-                {formattedAmount} {symbol}
-              </b>{" "}
-              -{formattedMessage} by {donation.user.id}
-            </Text>
-          </ListItem>
-        );
-      })}
-    </List>
+            return (
+              <ListItem
+                key={donation.id}
+                display={"flex"}
+                alignItems={"center"}
+              >
+                <ListIcon as={CircleStackIcon} color="green.500" />
+                <Text>
+                  <b>
+                    {formattedAmount} {symbol}
+                  </b>{" "}
+                  -{formattedMessage} by {donation.user.id}
+                </Text>
+              </ListItem>
+            );
+          })}
+      </List>
+    </>
   );
 }
 
