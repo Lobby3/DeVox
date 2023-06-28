ae97c46977a6d5f2252e5403d39ee3b3935b3481
jipstavenuiter <jipstavenuiter@gmail.com>
2023-06-28 11:30:59 +0200

    disable ssr for my donations page

--- packages/frontend/app/my-donations/page.tsx
+++ packages/frontend/app/my-donations/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import React from "react";
 
-import MyDonations from "../../components/my-donations/my-donations";
+const MyDonations = dynamic(
+  () => import("../../components/my-donations/my-donations"),
+  {
+    ssr: false,
+  }
+);
 
 export default async function Index() {
   return <MyDonations />;
